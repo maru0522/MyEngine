c7f144b8bf181b65cd4415cb635aeda9be2e6c4c
maru0522 <94513876+maru0522@users.noreply.github.com>
2023-06-28 15:33:21 +0900

    いったん評価課題最低分まではクリア。 3Dオブジェクトのほうはマルチレンダーターゲットは両方とも通常描画にした。

--- ModelPS.hlsl
+++ ModelPS.hlsl
@@ -129,6 +129,6 @@ PSOutput main(VSOutput input)
     
     // シェーディングによる色で描画
     output.target0 = shadecolor * texcolor;
-    output.target1 = float4(1 - (shadecolor * texcolor).rgb, 1);
+    output.target1 = shadecolor * texcolor;
     return output;
 }
