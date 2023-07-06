25a7c8e81d3f4f334ec2992863aba93a9f349c48
maru0522 <94513876+maru0522@users.noreply.github.com>
2023-07-06 10:17:19 +0900

    へっだのusing namespace　削除

--- SimplifyImGui.cpp
+++ SimplifyImGui.cpp
@@ -1,22 +1,22 @@
 #include "SimplifyImGui.h"
 
-void GUI::Begin(const string& windowName, const ImVec2& firstSize)
+void GUI::Begin(const std::string& windowName, const ImVec2& firstSize)
 {
     ImGui::SetNextWindowSize(firstSize, ImGuiCond_FirstUseEver);
     ImGui::Begin(windowName.c_str());
 }
 
-void GUI::ChildFrameBegin(const string& windowName, const ImVec2& size)
+void GUI::ChildFrameBegin(const std::string& windowName, const ImVec2& size)
 {
     ImGui::BeginChildFrame(ImGui::GetID(windowName.c_str()), size);
 }
 
-bool GUI::ButtonTrg(const string& buttonName, const ImVec2& size)
+bool GUI::ButtonTrg(const std::string& buttonName, const ImVec2& size)
 {
     return ImGui::Button(buttonName.c_str(),size);
 }
 
-bool GUI::DropDownTrg(const string& dropdownName, int* currentNum, const char* const* items, int arraySize)
+bool GUI::DropDownTrg(const std::string& dropdownName, int* currentNum, const char* const* items, int arraySize)
 {
     return ImGui::Combo(dropdownName.c_str(), currentNum, items, arraySize);
 }
--- SimplifyImGui.h
+++ SimplifyImGui.h
@@ -4,18 +4,16 @@
 
 namespace GUI
 {
-    using namespace std;
-
     // begin - end
-    void Begin(const string& windowName, const ImVec2& firstSize = { 100,100 });
+    void Begin(const std::string& windowName, const ImVec2& firstSize = { 100,100 });
     inline void End(void) { ImGui::End(); }
-    void ChildFrameBegin(const string& windowName, const ImVec2& size = { 100,100 });
+    void ChildFrameBegin(const std::string& windowName, const ImVec2& size = { 100,100 });
     inline void ChildFrameEnd(void) { ImGui::EndChildFrame(); }
 
     // return bool
-    bool ButtonTrg(const string& buttonName, const ImVec2& size = { 0,0 });
-    bool DropDownTrg(const string& dropdownName, int* currentNum, const char* const* items, int arraySize);
+    bool ButtonTrg(const std::string& buttonName, const ImVec2& size = { 0,0 });
+    bool DropDownTrg(const std::string& dropdownName, int* currentNum, const char* const* items, int arraySize);
 
 
-    inline void CheckBox(const string& checkboxName, bool* v) { ImGui::Checkbox(checkboxName.c_str(), v); }
+    inline void CheckBox(const std::string& checkboxName, bool* v) { ImGui::Checkbox(checkboxName.c_str(), v); }
 }
