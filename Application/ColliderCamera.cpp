39e33d631f5096cbe56d0c620af1f61f89c933ab
maru0522 <94513876+maru0522@users.noreply.github.com>
2023-08-11 19:11:07 +0900

    警告で実行できないからいったんコメントアウト戻した

--- Application/ColliderCamera.cpp
+++ Application/ColliderCamera.cpp
@@ -46,14 +46,14 @@ void ColliderCamera::OnCollision(void)
 
 void ColliderCamera::CalcAxis3(const Vector3& playerPos, const Vector3& pUpVec)
 {
-    //axes_.forward = (playerPos - transform_.position).Normalize();
+    axes_.forward = (playerPos - transform_.position).Normalize();
 
-    //// プレイヤーの上ベクトルとの外積でカメラの右向きベクトルを定義。
-    //// カメラ位置が、プレイヤー上ベクトルのラインより下なら、正しく右向きが出る。
-    //axes_.right = pUpVec.Cross(axes_.forward).Normalize();
+    // プレイヤーの上ベクトルとの外積でカメラの右向きベクトルを定義。
+    // カメラ位置が、プレイヤー上ベクトルのラインより下なら、正しく右向きが出る。
+    axes_.right = pUpVec.Cross(axes_.forward).Normalize();
 
-    //// 右向きベクトルが反転してると下向きベクトルになってしまうから注意。
-    //axes_.up = axes_.forward.Cross(axes_.right).Normalize();
+    // 右向きベクトルが反転してると下向きベクトルになってしまうから注意。
+    axes_.up = axes_.forward.Cross(axes_.right).Normalize();
 
     // なんでこれだとうまくいくんや？行列からの各軸抜き出しとの違いを出すべきかも。
     axes_.forward = coordinate_.GetMatAxisZ();
