b2afc183bc4e87c15636b41f3f464be0ec711b78
maru0522 <94513876+maru0522@users.noreply.github.com>
2023-11-14 16:19:29 +0900

    IPrimitiveクラスの追加

---
+++ IPrimitive.h
@@ -0,0 +1,28 @@
+#pragma once
+
+enum class Shape
+{
+    UNKNOWN = -1, // 未定義
+    SPHERE,       // 球
+    PLANE,        // 平面
+    POINT,        // 点
+    AABB,         // XYZ軸に平行な直方体
+    OBB,          // 分離軸に平行な直方体
+    RAY,          // 半直線
+};
+
+class IPrimitive
+{
+public:
+    //>> 関数
+    IPrimitive(void) = default;
+    virtual ~IPrimitive(void) = default;
+
+protected:
+    Shape shape_;
+
+public:
+    // getter
+    Shape GetShape(void) { return shape_; }
+};
+
