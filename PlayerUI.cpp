d32b9c80aa7a2fc09d9cd826ecc62863217c229c
maru0522 <94513876+maru0522@users.noreply.github.com>
2023-11-17 16:30:25 +0900

    UIかんぺき~

--- PlayerUI.cpp
+++ PlayerUI.cpp
@@ -9,6 +9,7 @@ PlayerUI::~PlayerUI(void)
 
     uiPtr_->UnRegister("playerUI_starCount");
     uiPtr_->UnRegister("playerUI_coinCount");
+    uiPtr_->UnRegister("playerUI_starDustCount");
     uiPtr_->UnRegister("playerUI_remain");
     uiPtr_->UnRegister("playerUI_life");
 }
@@ -24,18 +25,30 @@ void PlayerUI::Initialize(void)
     // nullチェック
     if (!uiPtr_) { return; }
 
+    // 星の数の画像
     uiPtr_->Register("playerUI_starCount", "Resources/starCount.png");
     uiPtr_->GetUISpritePtr("playerUI_starCount")->SetScale(Vector2{ 0.4f,0.4f });
     uiPtr_->GetUISpritePtr("playerUI_starCount")->SetPosition(Vector2{ -80.f ,40.f });
     uiPtr_->GetUISpritePtr("playerUI_starCount")->SetAlpha(0.f);
 
+    // コインの数の画像
     uiPtr_->Register("playerUI_coinCount", "Resources/coinCount.png");
     uiPtr_->GetUISpritePtr("playerUI_coinCount")->SetScale(Vector2{ 0.4f,0.4f });
-    uiPtr_->GetUISpritePtr("playerUI_coinCount")->SetPosition(Vector2{ 1195.f, 600.f });
+    uiPtr_->GetUISpritePtr("playerUI_coinCount")->SetPosition(Vector2{ 1195.f, 580.f });
     uiPtr_->GetUISpritePtr("playerUI_coinCount")->SetAlpha(0.f);
 
-    uiPtr_->Register("playerUI_remain", "Resources/remain.png");
+    // 星屑の数の画像
+    uiPtr_->Register("playerUI_starDustCount", "Resources/starDustCount.png");
+    uiPtr_->GetUISpritePtr("playerUI_starDustCount")->SetScale(Vector2{ 0.4f,0.4f });
+    uiPtr_->GetUISpritePtr("playerUI_starDustCount")->SetPosition(Vector2{ 1080.f, 640.f });
 
+    // 残機の数の画像
+    uiPtr_->Register("playerUI_remain", "Resources/remain.png");
+    uiPtr_->GetUISpritePtr("playerUI_remain")->SetScale(Vector2{ 0.4f,0.4f });
+    uiPtr_->GetUISpritePtr("playerUI_remain")->SetPosition(Vector2{ -80.f ,640.f });
+    uiPtr_->GetUISpritePtr("playerUI_remain")->SetAlpha(0.f);
+
+    // HPの画像
     uiPtr_->Register("playerUI_life", "Resources/life.png");
     uiPtr_->GetUISpritePtr("playerUI_life")->SetScale(Vector2{ 1.f,1.f });
     uiPtr_->GetUISpritePtr("playerUI_life")->SetPosition(Vector2{ 1060.f + 115.f, 50.f });
@@ -52,9 +65,11 @@ void PlayerUI::Update(void)
 
     uiPtr_->Update("playerUI_starCount");
     uiPtr_->Update("playerUI_coinCount");
+    uiPtr_->Update("playerUI_starDustCount");
     uiPtr_->Update("playerUI_remain");
     uiPtr_->Update("playerUI_life");
 
+    // imgui
     GUI::Begin("PlayerUI");
     GUI::CheckBox("is_visible_", &is_visible_);
     GUI::Text("rate:  %f", easeTimer_.GetTimeRate());
@@ -71,21 +86,13 @@ void PlayerUI::Draw(void)
 
     uiPtr_->Draw("playerUI_starCount");
     uiPtr_->Draw("playerUI_coinCount");
-    //uiPtr_->Draw("playerUI_remain");
+    uiPtr_->Draw("playerUI_starDustCount");
+    uiPtr_->Draw("playerUI_remain");
     uiPtr_->Draw("playerUI_life");
 }
 
 void PlayerUI::EaseUI(void)
 {
-    //if(IsTrigger() && is_end_)
-    //{
-    //    is_end_ = false;
-
-    //    easeTimer_.Start(1.f);
-    //}
-
-    //if (IsExecute() == false) { return; }
-
     if (IsTrigger() || IsRelease())
     {
         easeTimer_.Start(0.6f);
@@ -96,18 +103,28 @@ void PlayerUI::EaseUI(void)
     {
         easeTimer_.Update();
 
+        // 星の数の画像
         const Vector2& starCount_defPos = { -80.f, 40.f };
         const float starCount_posX = Math::Ease::EaseOutSine(easeTimer_.GetTimeRate(), starCount_defPos.x, starCount_defPos.x + 115.f);
         uiPtr_->GetUISpritePtr("playerUI_starCount")->SetPosition(Vector2{ starCount_posX,starCount_defPos.y });
         const float starCount_alpha = Math::Ease::EaseOutSine(easeTimer_.GetTimeRate());
         uiPtr_->GetUISpritePtr("playerUI_starCount")->SetAlpha(starCount_alpha);
 
-        const Vector2& coinCount_defPos = { 1195.f, 600.f };
+        // コインの数の画像
+        const Vector2& coinCount_defPos = { 1195.f, 580.f };
         const float coinCount_posX = Math::Ease::EaseOutSine(easeTimer_.GetTimeRate(), coinCount_defPos.x, coinCount_defPos.x - 115.f);
         uiPtr_->GetUISpritePtr("playerUI_coinCount")->SetPosition(Vector2{ coinCount_posX,coinCount_defPos.y });
         const float coinCount_alpha = Math::Ease::EaseOutSine(easeTimer_.GetTimeRate());
         uiPtr_->GetUISpritePtr("playerUI_coinCount")->SetAlpha(coinCount_alpha);
 
+        // 残機の数の画像
+        const Vector2& remain_defPos = { -80.f ,640.f };
+        const float remain_posX = Math::Ease::EaseOutSine(easeTimer_.GetTimeRate(), remain_defPos.x, remain_defPos.x + 115.f);
+        uiPtr_->GetUISpritePtr("playerUI_remain")->SetPosition(Vector2{ remain_posX,remain_defPos.y });
+        const float remain_alpha = Math::Ease::EaseOutSine(easeTimer_.GetTimeRate());
+        uiPtr_->GetUISpritePtr("playerUI_remain")->SetAlpha(remain_alpha);
+
+        // HPの画像
         const Vector2& life_defPos = { 1175.f, 50.f };
         const float life_posX = Math::Ease::EaseOutSine(easeTimer_.GetTimeRate(), life_defPos.x, life_defPos.x - 115.f);
         uiPtr_->GetUISpritePtr("playerUI_life")->SetPosition(Vector2{ life_posX,life_defPos.y });
@@ -118,18 +135,28 @@ void PlayerUI::EaseUI(void)
     {
         easeTimer_.Update();
 
+        // 星の数の画像
         const Vector2& starCount_defPos = { 35.f, 40.f };
         const float starCount_posX = Math::Ease::EaseOutSine(easeTimer_.GetTimeRate(), starCount_defPos.x, starCount_defPos.x - 115.f);
         uiPtr_->GetUISpritePtr("playerUI_starCount")->SetPosition(Vector2{ starCount_posX,starCount_defPos.y });
         const float starCount_alpha = Math::Ease::EaseOutSine(easeTimer_.GetTimeRate(),1.f,0.f);
         uiPtr_->GetUISpritePtr("playerUI_starCount")->SetAlpha(starCount_alpha);
 
-        const Vector2& coinCount_defPos = { 1080.f, 600.f };
+        // コインの数の画像
+        const Vector2& coinCount_defPos = { 1080.f, 580.f };
         const float coinCount_posX = Math::Ease::EaseOutSine(easeTimer_.GetTimeRate(), coinCount_defPos.x, coinCount_defPos.x + 115.f);
         uiPtr_->GetUISpritePtr("playerUI_coinCount")->SetPosition(Vector2{ coinCount_posX,coinCount_defPos.y });
         const float coinCount_alpha = Math::Ease::EaseOutSine(easeTimer_.GetTimeRate(), 1.f, 0.f);
         uiPtr_->GetUISpritePtr("playerUI_coinCount")->SetAlpha(coinCount_alpha);
 
+        // 残機の数の画像
+        const Vector2& remain_defPos = { 35.f ,640.f };
+        const float remain_posX = Math::Ease::EaseOutSine(easeTimer_.GetTimeRate(), remain_defPos.x, remain_defPos.x - 115.f);
+        uiPtr_->GetUISpritePtr("playerUI_remain")->SetPosition(Vector2{ remain_posX,remain_defPos.y });
+        const float remain_alpha = Math::Ease::EaseOutSine(easeTimer_.GetTimeRate());
+        uiPtr_->GetUISpritePtr("playerUI_remain")->SetAlpha(remain_alpha);
+
+        // HPの画像
         const Vector2& life_defPos = { 1060.f, 50.f };
         const float life_posX = Math::Ease::EaseOutSine(easeTimer_.GetTimeRate(), life_defPos.x, life_defPos.x + 115.f);
         uiPtr_->GetUISpritePtr("playerUI_life")->SetPosition(Vector2{ life_posX,life_defPos.y });
