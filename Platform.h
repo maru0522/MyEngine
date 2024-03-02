1ac762400f11f6e4f6427411af8b207dd21364ab
maru0522 <94513876+maru0522@users.noreply.github.com>
2024-03-02 11:32:20 +0900

    足場クラス入れたけどいったん放置かも

---
+++ Platform.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Object3D.h"
+#include "CollisionManager.h"
+#include "CollisionPrimitive.h"
+
+class Platform
+{
+public:
+    // 関数
+    Platform(void) = default;
+    ~Platform(void) = default;
+
+
+};
+
